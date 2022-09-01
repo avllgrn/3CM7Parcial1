3b5487ed8607d79d2009d32e96911e8d70503915
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-09-01 11:43:27 -0500

    Menu base

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 class Nodo{
 private:
-    string dato;
+    int dato;
     Nodo* inferior;
 public:
     Nodo(void);
-    Nodo(string d, Nodo* i);
+    Nodo(int d, Nodo* i);
     void muestraDatos(void);
     void muestraDato(void);
     void pideDatos(void);
-    string retornaDato(void);
-    void modificaDato(string d);
+    int retornaDato(void);
+    void modificaDato(int d);
     Nodo* retornaInferior(void);
     void modificaInferior(Nodo* i);
 };
 Nodo::Nodo(void){
+    dato = 0;
     inferior = NULL;
 }
-Nodo::Nodo(string d, Nodo* i){
+Nodo::Nodo(int d, Nodo* i){
     dato = d;
     inferior = i;
 }
@@ -38,10 +40,10 @@ void Nodo::muestraDato(void){
 void Nodo::pideDatos(void){
     cout<<"Dame mi dato: ";cin>>dato;
 }
-string Nodo::retornaDato(void){
+int Nodo::retornaDato(void){
     return dato;
 }
-void Nodo::modificaDato(string d){
+void Nodo::modificaDato(int d){
     dato = d;
 }
 Nodo* Nodo::retornaInferior(void){
@@ -53,45 +55,64 @@ void Nodo::modificaInferior(Nodo* i){
 
 int main(void){
     Nodo* tope;
-
-    tope = NULL;//Pila vacia
-
-    tope = new Nodo("Una", tope);    //Push a la pila, entra "Una" primero
-    tope = new Nodo("cadena", tope);    //Push a la pila, entra "cadena"
-    tope = new Nodo("de", tope);    //Push a la pila, entra "de"
-    tope = new Nodo("caracteres", tope);   //Push a la pila, entra "caracteres" al final
-
-
     Nodo* aux;
-    string d;
+    int op,d;
 
-    //Pop a la pila, sale "caracteres" primero (ultimo en entrar)
-    d = tope->retornaDato();
-    aux = tope;
-    tope = tope->retornaInferior();
-    delete aux;
-    cout << d << endl;
+    tope = NULL;
 
-    //Pop a la pila, sale "de"
-    d = tope->retornaDato();
-    aux = tope;
-    tope = tope->retornaInferior();
-    delete aux;
-    cout << d << endl;
+    do{
+        system("cls");
+        cout<<"1. push"<<endl
+            <<"2. pop"<<endl
+            <<"3. elimina Pila"<<endl
+            <<"4. Salir"<<endl
+            <<"Cual es tu opcion? ";
+        cin>>op;
+        system("cls");
+        switch(op){
+            case 1:
+                cout<<"Ingresa dato ";cin>>d;
+                tope = new Nodo(d,tope);
+                break;
+            case 2:
+                if(tope==NULL){
+                    cout<<"La pila esta vacia... =("<<endl;
+                }
+                else{
+                    d = tope->retornaDato();
+                    aux = tope;
+                    tope = tope->retornaInferior();
+                    delete aux;
+                    cout<<"Salio nodo con dato "<<d<<endl<<endl;
+                }
+                break;
+            case 3:
+                while(tope!=NULL){
+                    d = tope->retornaDato();
+                    aux = tope;
+                    tope = tope->retornaInferior();
+                    delete aux;
+                    cout<<"Salio nodo con dato "<<d<<endl<<endl;
+                }
+                break;
+            case 4:
+                cout<<"Adios! =)"<<endl<<endl;
+                while(tope!=NULL){
+                    d = tope->retornaDato();
+                    aux = tope;
+                    tope = tope->retornaInferior();
+                    delete aux;
+                    cout<<"Salio nodo con dato "<<d<<endl<<endl;
+                }
+                break;
+            default :
+                cout<<"Opcion invalida...! =("<<endl<<endl;
+                break;
+        }
+        if(op!=4)
+            system("pause");
 
-    //Pop a la pila, sale "cadena"
-    d = tope->retornaDato();
-    aux = tope;
-    tope = tope->retornaInferior();
-    delete aux;
-    cout << d << endl;
-
-    //Pop a la pila, sale "Una" al final  (primero en entrar)
-    d = tope->retornaDato();
-    aux = tope;
-    tope = tope->retornaInferior();
-    delete aux;
-    cout << d << endl;
+    }while(op!=4);
 
     return 0;
 }
